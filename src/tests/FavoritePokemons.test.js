8cccf3167982c2fce0cdcd01ad50a8d047a3ce33
yurigarrido <y.garrido.yuri@gmail.com>
2021-08-18 17:44:36 -0300

    envia o requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utility/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente "<FavoritePokemons.js />"', () => {
+  it('testa se a pessoa não tiver pokémons favoritos é exibido uma mensagem', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const mensagePragraph = screen.getByText(/No favorite pokemon found/i);
+    expect(mensagePragraph).toBeInTheDocument();
+  });
+});
