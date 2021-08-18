943b9d85c6f2ad79289caeb831a684c6dfe72915
yurigarrido <y.garrido.yuri@gmail.com>
2021-08-18 14:51:43 -0300

    cria a pasta ultility com o arquivo renderWithRouter e faz os testes do header

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utility/renderWithRouter';
+
+describe('Testa se o topo da aplicação contém um conjunto de links de navegação.', () => {
+  it('verifica se o primeiro link possui o texto "Home"', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', {
+      name: 'Home' });
+    expect(home).toBeInTheDocument();
+  });
+  it('verifica se o segundo link possui o texto "About"', () => {
+    renderWithRouter(<App />);
+    const about = screen.getByRole('link', {
+      name: 'About' });
+    expect(about).toBeInTheDocument();
+  });
+  it('verifica se o terceiro link possui o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+    const favoritePokemons = screen.getByRole('link', {
+      name: 'Favorite Pokémons' });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+});
