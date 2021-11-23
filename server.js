eeaa2a1cceb05c01f585905f0a33db7b1871fc21
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-23 00:00:23 -0300

    heroku setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const { resolve } = require('path')
+  app.use('/', express.static(
+    resolve(
+      __dirname,
+      './build'
+    )
+  )
+)
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) { return console.log(err); }
+  console.log('tudo funcionando certinho');
+})
